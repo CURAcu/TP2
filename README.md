ce74f3322d4b230740f7f54f99e25bf45595b18d
Candela Dargoltz <soycurabot@gmail.com>
2026-02-03 23:57:39 -0300

    TP2

---
+++ README.md
@@ -0,0 +1,25 @@
+# Trabajo Práctico N°2 – Backend + Frontend
+
+El proyecto cuenta con:
+- Backend en Node.js + Express + MongoDB
+- Frontend en React + Vite
+- Autenticación con JWT
+- Rutas protegidas
+- Gestión de Workspaces
+
+## Requisitos
+
+- Node.js instalado
+- MongoDB Atlas
+
+## Instalación
+
+Ingresar a la carpeta backend:
+
+cd backend
+npm install
+node main.js
+
+cd frontend
+npm install
+npm run dev
---
+++ tp2-backend/main.js
@@ -0,0 +1,27 @@
+
+import { connectMongoDB } from "./config/mongoDB.config.js"
+import express from 'express'
+import authRouter from "./routes/auth.router.js"
+import randomMiddleware from "./middlewares/random.middleware.js"
+import cors from 'cors'
+import workspaceRouter from "./routes/workspace.router.js"
+import workspaceRepository from "./repository/workspace.repository.js"
+import messagesRepository from "./repository/messages.repository.js"
+
+connectMongoDB()
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use("/api/auth", authRouter)
+app.use("/api/workspace", workspaceRouter)
+
+app.listen(
+    8080, 
+    () => {
+        console.log('Nuestra app se escucha en el puerto 8080')
+    }
+)
