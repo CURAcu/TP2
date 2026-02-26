9c0bfd3c1e607eff8df307285489d5fa160f60ec
Candela Dargoltz <soycurabot@gmail.com>
2026-02-26 06:32:53 -0300

    useregister

--- tp2-frontend/src/hooks/useRegister.jsx
+++ tp2-frontend/src/hooks/useRegister.jsx
@@ -2,38 +2,30 @@ import { register } from "../services/authService"
 import useForm from "./useForm"
 import useRequest from "./useRequest"
 
-if (!form_state.username?.trim()) return
-if (!form_state.email?.trim()) return
-if (!form_state.password?.trim()) return
-if (form_state.password.trim().length < 6) return
-
-function useRegister (){
-    const {loading, error, response, sendRequest} = useRequest()
+function useRegister() {
+    const { loading, error, response, sendRequest } = useRequest()
 
     const form_initial_state = {
-        username: '',
-        password: '',
-        email: ''
+        username: "",
+        password: "",
+        email: "",
     }
 
-    async function enviarRegistro (form_state){
-        sendRequest(
-            () => {
-                return register(form_state.username, form_state.password, form_state.email)
-            }
-        )
+    async function enviarRegistro(form_state) {
+        if (!form_state.username?.trim()) return
+        if (!form_state.email?.trim()) return
+        if (!form_state.password?.trim()) return
+        if (form_state.password.trim().length < 6) return
+
+        return sendRequest(() => {
+            return register(form_state.username, form_state.password, form_state.email)
+        })
     }
 
-    const {
-        form_state,
-        onChangeFieldValue,
-        onSubmitForm
-    } = useForm(
-        {
-            initial_form_fields: form_initial_state,
-            onSubmit: enviarRegistro
-        }
-    )
+    const { form_state, onChangeFieldValue, onSubmitForm } = useForm({
+        initial_form_fields: form_initial_state,
+        onSubmit: enviarRegistro,
+    })
 
     return {
         form_state,
@@ -41,7 +33,7 @@ function useRegister (){
         onSubmitForm,
         loading,
         error,
-        response
+        response,
     }
 }
 
